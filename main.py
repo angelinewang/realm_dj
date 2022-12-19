4d7aa86623d21d1168d7b7e3e54428eecac67ba9
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-12-19 18:08:00 +0000

    PORT in main.py to 8080

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 
-app.run(port=int(os.environ.get("PORT", 3000)), host='0.0.0.0', debug=False)
+app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=False)
 
 # @app.route("/")
 # def hello_world():
