ac407e95c67a6351e6325740533f453db1d5dbba
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-12-20 13:49:54 +0000

    DB Host and Port and .env

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
 #     name = os.environ.get("NAME", "World")
 #     return "Hello {}!".format(name)
 
-
 # if __name__ == "__main__":
 #     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
     # app.run(port=int(os.environ.get("DB_PORT", "8000")),
