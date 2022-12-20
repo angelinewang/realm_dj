99e4336d944fea673538082d586b2af5663e5517
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-12-20 22:32:54 +0000

    Flask, SQLAlchemy connect to postgres database

--- base/__init__.py
+++ base/__init__.py
@@ -1,7 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from google.cloud.sql.connector import Connector, IPTypes
+
+
+# Python Connector database connection function
+def getconn():
+    with Connector() as connector:
+        conn = connector.connect(
+            "realm-rn-dj:europe-west1:realm-django",  # Cloud SQL Instance Connection Name
+            "pg8000",
+            user="postgres",
+            password="blueLilacs8830",
+            db="realm_django",
+            ip_type=IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+        )
+        return conn
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+# configure Flask-SQLAlchemy to use Python Connector
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+pg8000://postgres:blueLilacs8830@35.195.57.236:5432/realm_django"
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "creator": getconn
+}
+
+db = SQLAlchemy(app)
