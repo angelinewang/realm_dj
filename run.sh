bbba4bddfd7e43dc5c160b186d3700d49794e2c6
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-12-27 16:32:09 +0000

    gunicorn command move base.wsgi:application to end and change back to main in Docker

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@
 sleep 1
 
 # Start the server 
-/usr/local/bin/gunicorn base.wsgi:application -w 2 -b :8080 app:app
+/usr/local/bin/gunicorn -w 2 -b :8080 base.wsgi:application
 
 # Testing with service account cloudrun
