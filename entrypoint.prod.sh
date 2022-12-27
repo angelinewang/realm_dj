0802740a706b3288f8059afa31ab851dc627596a
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2022-12-27 16:40:27 +0000

    Change application to app in run.sh

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+# #!/bin/sh
 
-if [ "$DATABASE" = "realm_django" ]
-then
-    echo "Waiting for realm_django..."
+# if [ "$DATABASE" = "realm_django" ]
+# then
+#     echo "Waiting for realm_django..."
 
-    while ! nc -z $DB_HOST $DB_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $DB_HOST $DB_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "realm_django started"
-fi
+#     echo "realm_django started"
+# fi
 
-exec "$@"
+# exec "$@"
--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@
 sleep 1
 
 # Start the server 
-/usr/local/bin/gunicorn -w 2 -b :8080 base.wsgi:application
+/usr/local/bin/gunicorn -w 2 -b :8080 base.wsgi:app
 
 # Testing with service account cloudrun
