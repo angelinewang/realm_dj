6b1fae538777e0a5d79753a959a26838137277b5
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-01-18 10:03:41 +0000

    Add back run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./cloud_sql_proxy -instances=realm-rn-dj:europe-west1:realm-django=tcp:8000 -credential_file=secrets/db-proxy.json &
+
+# wait for proxy to spin up
+sleep 1
+
+# Start the server 
+/usr/local/bin/gunicorn/main:app -w 2 -b :8080 
+
+# After PORT, must run the "application" variable inside base.wsgi
+# This is often remained main for App Engine
+
+# Testing with service account cloudrun
