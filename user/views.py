e5d1328349fc052cbfdec70db6eab477b660a903
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-01-31 11:44:37 +0000

    Attempt save both profile_picture and profile_picture_data

--- user/views.py
+++ user/views.py
@@ -47,7 +47,7 @@ class RegisterView(generics.CreateAPIView):
         # If email entered already exists, will through Error: "Key (email)=(testing@kcl.ac.uk) already exists."
         # print(request.data.get('email'))
         profile_picture = request.data.get('profile_picture')
-        profile_picture_data = request.cleaned_data['profile_picture'].file.read(
+        profile_picture_data = request.data.get['profile_picture'].file.read(
         )
         email = request.data.get('email')
         password = request.data.get('password')
