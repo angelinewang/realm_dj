bad37c7b512b5b4852bbf4d0e65457414cae72f4
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-01-31 09:33:10 +0000

    Create profile_picture FileField and profile_picture_data BinaryField

--- user/models.py
+++ user/models.py
@@ -65,7 +65,8 @@ class CustomUser(AbstractUser):
     # validators = [validate_file_extension]
     # profile_picture = models.ImageField(
     #     null=True, storage=fs)
-    profile_picture = models.CharField(null=True, blank=True, max_length=1000)
+    profile_picture = models.FileField()
+    profile_picture_data = models.BinaryField(null=True)
     # url = models.CharField(max_length=255, unique=True)
     role = models.IntegerField(choices=[x.value for x in ROLE], default=ROLE.get_value(ROLE.guest))
 
