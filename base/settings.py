99df33ff59f0313c022270f0472ce3ecaac3f3a1
ANGELINE WANG <02ANGELINEWANG@GMAIL.COM>
2023-02-17 09:28:59 +0000

    Debug True is working without 500, so set Debug to env, and added False Debug env to Cloud Run

--- base/settings.py
+++ base/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'giyBtGiAiTKpsRxgNasNhDOCCglcPTxUGYJcmppgZzqgMujZAn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = [
     
